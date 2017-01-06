f3a3735e831c90193929578683f85acdd6f4c8cf
pthaks <priya.a.thakur@gmail.com>
2017-01-06 12:40:06 +0530

    first commit

---
+++ src/main/java/org/pthaks/demo/TFunc.java
@@ -0,0 +1,13 @@
+package org.pthaks.demo;
+
+import org.testng.annotations.Test;
+
+public class TFunc {
+	
+	@Test
+	public void first()
+	{
+		System.out.println("Within the TESTNG class............. Priya");
+	}
+
+}
